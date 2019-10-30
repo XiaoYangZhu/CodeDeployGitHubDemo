0aac3bf152aa9f964218cedeba1f055bb701cec4
Zhu <xiaoyzhu@88e9fe843ac1.ant.amazon.com>
2019-10-30 16:59:52 +0800

    fix the handler code

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
+        headers: {
+          "Content-Type": "text/html"
+        },
         statusCode: '200',
         body: 'The time in Los Angeles now is: ' + currentTime.toString() + ' .',
     });
